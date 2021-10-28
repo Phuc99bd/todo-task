1be2bb0a955cb95e0269f130ce10181f1f1ac400
congphuc <phuccog@gmail.com>
2021-10-28 10:04:39 +0700

    grpc server

--- src/modules/todo/todo.module.ts
+++ src/modules/todo/todo.module.ts
@@ -5,24 +5,8 @@ import { Todo, TodoSchema } from './schemas/todo.schema';
 import { ConfigService,ConfigModule } from '@nestjs/config';
 import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 import { join } from 'path';
-import { TodoController } from './todo.controller';
 
 @Module({
-  // providers: [
-  //   {
-  //     provide: 'TODO_PACKAGE',
-  //     useFactory: (configService: ConfigService) => {
-  //       return ClientProxyFactory.create({
-  //         transport: Transport.GRPC,
-  //         options: {
-  //           package: 'todos',
-  //           protoPath: join(process.cwd(), 'src/modules/todo/todo.proto'),
-  //         },
-  //       })
-  //     },
-  //     inject: [ConfigService],
-  //   },
-  // ],
   controllers: [TodoService],
   imports: [ConfigModule , MongooseModule.forFeature([{ name: Todo.name , schema: TodoSchema }])]
 })
